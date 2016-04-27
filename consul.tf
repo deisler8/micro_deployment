27c8f857150f231b1d074677db6e6f716276da0d
lilu3 <lilu3@cisco.com>
2016-04-27 10:36:12 -0400

    change to use gliderlabs consul

--- consul.tf
+++ consul.tf
@@ -1,15 +1,33 @@
-# Create a container
-resource "docker_container" "consul1" {
-    image = "${docker_image.consul.latest}"
-    name = "consul1"
-    command = ["/usr/local/bin/docker-entrypoint.sh", "agent", "-ui"]
-    ports {
-        internal = "8500"
-        external = "8500"
-        ip = "localhost"
-    }
+resource "docker_image" "consul" {
+  name = "gliderlabs/consul-server:latest"
+  keep_updated = true
 }
 
-resource "docker_image" "consul" {
-    name = "consul:latest"
+# Create a container
+resource "docker_container" "consul-server1" {
+  image = "${docker_image.consul.latest}"
+  name = "consul-server1"
+  restart = "always"
+  must_run = true
+
+  ports {
+    internal = 53
+    external = 53
+    protocol = "udp"
+  }
+  ports {
+    internal = 8500
+    external = 8500
+  }
+
+  command = ["-server", "-bootstrap-expect", "2"]
+}
+
+resource "docker_container" "consul-server2" {
+  image = "${docker_image.consul.latest}"
+  name = "consul-server2"
+  restart = "always"
+  must_run = true
+
+  command = ["-server", "-join", "172.17.0.2"]
 }
