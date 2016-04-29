6b2f3f9059aa560dfbb7c3b8693c8c0fe6898582
lilu3 <lilu3@cisco.com>
2016-04-29 18:20:35 -0400

    more

--- spark/spark.tf
+++ spark/spark.tf
@@ -39,5 +39,32 @@ resource "docker_container" "spark-master" {
     host_path = "/data"
   }
 
-  command = ["bin/spark-class", "org.apache.spark.deploy.master.Master", "-h", "master"]
+  #command = ["bin/spark-class", "org.apache.spark.deploy.master.Master", "-h", "master"]
+}
+
+resource "docker_container" "spark-worker" {
+  depends_on = ["docker_container.spark-master"]
+  image = "${docker_image.spark.latest}"
+  name = "spark-worker"
+  restart = "always"
+  must_run = true
+
+  ports {
+    internal = 8081
+    external = 8081
+  }
+
+  env = ["SPARK_CONF_DIR=/conf", "SPARK_WORKER_CORES=2", "SPARK_WORKER_MEMORY=1g", "SPARK_WORKER_PORT=8081", "SPARK_WORKER_WEBUI_PORT=8081"]
+
+  volumes {
+    container_path = "/conf"
+    host_path = "/conf/worker"
+  }
+
+  volumes {
+    container_path = "/tmp/data"
+    host_path = "/data"
+  }
+
+  command = ["bin/spark-class", "org.apache.spark.deploy.worker.Worker", "spark://master:7077"]
 }
